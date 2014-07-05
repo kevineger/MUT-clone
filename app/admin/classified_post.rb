d59692f2c7380414224fa897d775678ab9f9d59a
jakedavies <jgdavies24@gmail.com>
2014-07-05 16:26:35 -0700

    fixed the bugs

--- app/admin/classified_post.rb
+++ app/admin/classified_post.rb
@@ -1,6 +1,4 @@
 ActiveAdmin.register ClassifiedPost do
-
-  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -13,9 +11,9 @@ ActiveAdmin.register ClassifiedPost do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  index do
-    column
-    column :classified_category_id
+  controller do
+    def permitted_params
+      params.permit!
+    end
   end
-  
 end
