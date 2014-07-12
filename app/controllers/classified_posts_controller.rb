4f60e2e072c3da6641b44a3a189d51c1e9243426
jakedavies <jgdavies24@gmail.com>
2014-07-12 14:32:27 -0700

    search 50% there

--- app/controllers/classified_posts_controller.rb
+++ app/controllers/classified_posts_controller.rb
@@ -66,12 +66,22 @@ class ClassifiedPostsController < ApplicationController
     if !page
       page = 1
     end
+
     @category = params[:cat_id]
+    @search = params[:search]
     @posts
-    if(@category == '0')
-      @posts = ClassifiedPost.all.paginate(:page => page)
+    if(@search==''||!@search)
+      if(@category == ''|| !@category||@category=='0')
+        @posts = ClassifiedPost.all.paginate(:page => page)
+      else
+        @posts = ClassifiedCategory.find(@category).classified_posts.paginate(:page => page)
+      end
     else
-      @posts = ClassifiedCategory.find(@category).classified_posts.paginate(:page => page)
+      if(@category == ''|| !@category||@category=='0')
+        @posts = ClassifiedPost.where(["title like ?", @search]).paginate(:page => page)
+      else
+        @posts = ClassifiedCategory.find(@category).classified_posts.where(["title like ?", @search]).paginate(:page => page)
+      end
     end
     respond_to do |format|
       format.js
