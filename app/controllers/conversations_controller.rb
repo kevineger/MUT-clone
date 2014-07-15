2fb1afb4b3847766d8b5e16e1a09e09d1a4618b9
jakedavies <jgdavies24@gmail.com>
2014-07-14 19:54:03 -0700

    messaging partially implmented

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -14,6 +14,12 @@ class ConversationsController < ApplicationController
 
   # GET /conversations/new
   def new
+    @send_to = User.find(params[:user])
+    @subject
+    if params.has_key?(:post)
+      @post = ClassifiedPost.find(params[:post])
+      @subject = 'A message about your posting in the classifieds for: ' + @post.title
+    end
     @conversation = Conversation.new
     @conversation.messages.build
   end
@@ -25,8 +31,9 @@ class ConversationsController < ApplicationController
   # POST /conversations
   # POST /conversations.json
   def create
-    @conversation = Conversation.new(conversation_params)
-
+    @conversation = Conversation.new(conversation_params.except(:user_id))
+    @conversation.users << current_user
+    @conversation.users << User.find(conversation_params[:user_id])
     respond_to do |format|
       if @conversation.save
         format.html { redirect_to @conversation, notice: 'Conversation was successfully created.' }
@@ -70,6 +77,6 @@ class ConversationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def conversation_params
-      params.require(:conversation).permit(:name,:subject,  messages_attributes: [:body])
+      params.require(:conversation).permit(:name,:subject, :user_id,  messages_attributes: [:body])
     end
 end
--- db/migrate/20140713042136_create_users_conversations_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersConversationsJoinTable < ActiveRecord::Migration
-  def change
-    create_table :users_conversations do |t|
-      t.integer :message_id
-      t.integer :user_id
-    end
-  end
-end
