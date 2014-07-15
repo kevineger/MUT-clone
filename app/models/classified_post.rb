0e755834c4cac30200de53ba5275e74cbb16d788
jakedavies <jgdavies24@gmail.com>
2014-07-14 23:44:15 -0700

    edited mode

--- app/models/classified_post.rb
+++ app/models/classified_post.rb
@@ -4,6 +4,12 @@ class ClassifiedPost < ActiveRecord::Base
 
   self.per_page = 21
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "200x200" ,:large => "500x500"}, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :bucket => 'mut-heroku-rails-paperclip',
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+                    :styles => { :medium => "300x300>" ,:large => "500x500"},
+                    :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  def s3_credentials
+    {:bucket => "mut-heroku-rails-paperclip", :access_key_id => "AKIAJ6FF7BNRUTRX2D3A", :secret_access_key => "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"}
+  end
 end
