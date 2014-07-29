37dd8ddf2642167327920d2e173b0357514f8c58
jakedavies <jgdavies24@gmail.com>
2014-07-28 22:42:43 -0700

    added scoping to posts model to begin filters

--- app/models/classified_post.rb
+++ app/models/classified_post.rb
@@ -1,6 +1,13 @@
 class ClassifiedPost < ActiveRecord::Base
   belongs_to :classified_category
   belongs_to :user
+
+  #possible filters
+  scope :status, -> (status) { where status: status }
+  scope :price_upper, -> (price_lower) { where location_id: location_id }
+  scope :price_lower, -> (price_upper) { where }
+  scope :category, -> (category_id) {where category_id: category_id}
+  scope :search, -> (title) { where("title like ?", "#{title}%")}
   self.per_page = 21
   has_attached_file :image,
                     :styles => { :thumb => "100x100>", :small => "250x250" ,:large => "500x500"},
