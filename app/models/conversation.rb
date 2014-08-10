587152d984042d801bde7efb600fea46973b46d8
jakedavies <jgdavies24@gmail.com>
2014-08-10 13:34:03 -0700

    merged kevin in

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,5 @@
 class Conversation < ActiveRecord::Base
   default_scope { order('created_at DESC') }
-  validates :name,:subject, presence: true
   has_and_belongs_to_many :users
   has_many :messages
   belongs_to :classified_post
