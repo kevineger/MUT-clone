0e7d084230743db9b56603f507e13ef0009610d6
Kevin <kevin.eger@hotmail.com>
2014-09-10 17:02:15 -0700

    Made profile card for non edit viewing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140810195221) do
+ActiveRecord::Schema.define(version: 20140909234548) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -44,11 +44,11 @@ ActiveRecord::Schema.define(version: 20140810195221) do
     t.integer  "classified_category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "expiry"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.datetime "expiry"
+    t.datetime "image_update_at"
   end
 
   create_table "conversations", force: true do |t|
