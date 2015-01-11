cd5ca0c5efbbb8244566d921761382394d23c564
jakedavies <jgdavies24@gmail.com>
2015-01-11 14:42:44 -0800

    messages link corrected

--- app/mailers/message_notifier.rb
+++ app/mailers/message_notifier.rb
@@ -2,10 +2,12 @@ class MessageNotifier < ActionMailer::Base
   default from: "noreply@myunitrade.com"
   def send_first_message_notification(to, conversation)
     @conversation = conversation
+    @to = to
     mail(:to => to.email, :subject => 'Your received a message about '+@conversation.subject)
   end
   def send_reply_notification(to, message)
     @message = message
+    @to = to
     mail(:to => to.email, :subject => 'You have received a reply about '+@message.conversation.subject)
   end
 end
