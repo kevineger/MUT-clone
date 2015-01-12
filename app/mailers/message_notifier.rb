ee70dd4b554cc248c7b8b68db254c1ceb870d3ac
jakedavies <jgdavies24@gmail.com>
2015-01-11 20:58:08 -0800

    updated email templates

--- app/mailers/message_notifier.rb
+++ app/mailers/message_notifier.rb
@@ -1,13 +1,15 @@
 class MessageNotifier < ActionMailer::Base
   default from: "noreply@myunitrade.com"
-  def send_first_message_notification(to, conversation)
+  def send_first_message_notification(to,from, conversation)
     @conversation = conversation
     @to = to
+    @from = from
     mail(:to => to.email, :subject => 'Your received a message about '+@conversation.subject)
   end
-  def send_reply_notification(to, message)
+  def send_reply_notification(to,from, message)
     @message = message
     @to = to
+    @from = from
     mail(:to => to.email, :subject => 'You have received a reply about '+@message.conversation.subject)
   end
 end
